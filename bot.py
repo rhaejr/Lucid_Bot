9b0ccd46c80b9cf3c4815a4d2828e3288d821a81
rhea <av8r.08@gmail.com>
2021-01-11 16:21:40 -0600

    working on createhchar function

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 from discord.ext.commands import CommandNotFound
 import sqlite3
 from sqlite3 import Error
+from datetime import date
+
 
 conn = sqlite3.connect("character.db")
 c = conn.cursor()
@@ -11,7 +13,8 @@ c = conn.cursor()
 table = """ CREATE TABLE IF NOT EXISTS characters (
                                         id integer PRIMARY KEY,
                                         character text NOT NULL,
-                                        player integer,
+                                        player text,
+                                        sheet text,
                                         birth_date text,
                                         total_dap integer
                                     ); """
@@ -42,9 +45,26 @@ async def on_ready():
     print("Bot is ready")
 
 @client.command()
-async def hello(ctx):
+async def hello(ctx,*args):
+    message = args[0]
+    mes = args[1]
+    mes = mes.replace('<@!', '')
+    mes = mes.replace('>','')
     n = ctx.author.display_name
-    await ctx.send('Hi! {}'.format(n))
+    out = 'Hi! {}, {} {}'.format(n,message,mes)
+    await ctx.send(out)
+
+@client.command()
+async def createchar(ctx,*args):
+    character, player = args
+    # player = player.replace('<@!', '')
+    # player = player.replace('>','')
+    sql = ''' INSERT INTO characters(character , player, birth_date, total_dap)
+              VALUES(?,?,?,?) '''
+    stuff = [character, player, date.today().strftime("%b-%d-%Y"), 0]
+    c.execute(sql,stuff)
+    conn.commit()
+    await ctx.send('Created')
 
 @client.event
 async def on_command_error(ctx, error):
